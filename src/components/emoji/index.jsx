21f1faca30d7a7d4063620ac6fbfaa16b0902693
Ankur Auti <115859079+Webdevava@users.noreply.github.com>
2023-06-05 20:15:17 +0530

    Update index.jsx

--- src/components/emoji/index.jsx
+++ src/components/emoji/index.jsx
@@ -1,26 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
 import LikeButton from './Like';
-import './styles.css'
-
+import './styles.css';
 
 const Emoji = (props) => {
+  const [newEmoji, setNewEmoji] = useState({ name: '', description: '', emoji: '' });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    props.onAddEmoji(newEmoji);
+    setNewEmoji({ name: '', description: '', emoji: '' });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewEmoji({ ...newEmoji, [name]: value });
+  };
+
   return (
     <div>
-        <h1>Emojipedia</h1>
-        <div className="emoji-list">
-          {
-            props.data.map (content =>(
-              <div className="list">
-                <dt><LikeButton/></dt>
-                <dt><span className="emoji"> {content.emoji}</span></dt>
-                <dt><span className="name">{content.name}</span></dt>
-                <dt><span className="meaning">{content.meaning}</span></dt>
-              </div>
-            ))
-          }
-        </div>
+      <h1>Emojipedia</h1>
+          <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="emoji"
+          placeholder="Emoji"
+          value={newEmoji.emoji}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={newEmoji.name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={newEmoji.description}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add Emoji</button>
+      </form>
+      <div className="emoji-list">
+        {props.data.map((content, index) => (
+          <div className="list" key={index}>
+            <dt><LikeButton /></dt>
+            <dd><span className="emoji">{content.emoji}</span></dd>
+            <dd><span className="name">{content.name}</span></dd>
+            <dd><span className="meaning">{content.meaning}</span></dd>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Emoji;
